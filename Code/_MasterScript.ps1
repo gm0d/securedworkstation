6e0613a12af7ab3ccfc5ac09c097138b1df43cf9
Guillermo Diaz <guillermo.diaz@mandiant.com>
2021-12-10 12:10:09 +1100

    update

--- Code/_MasterScript.ps1
+++ Code/_MasterScript.ps1
@@ -1,10 +1,6 @@
 #Requires -PSEdition Desktop
 #Requires -Modules @{ModuleName="AzureAD"; ModuleVersion="2.0.2" }, @{ModuleName="WindowsAutopilotIntune"; ModuleVersion="5.0"}, @{ModuleName="Microsoft.Graph"; ModuleVersion="1.8.0" }
-<#
-.COPYRIGHT
-Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
-See LICENSE in the project root for license information.
-#>
+
 [cmdletbinding()]
 param(        
     [ValidateSet("PAW","ENT","SPE")]
@@ -26,17 +22,40 @@ $User = (Connect-AzureAD).Account.Id
 Connect-MsGraph  
 ####################################################    
 
-#write-host "Adding App Registrtion"
-#. $ScriptDir/AppRegistration_Create.ps1
-#Start-Sleep -s 5
-
 # Get Auth token for Azure AD app id
 Write-Host "Adding required AAD Groups"
 Connect-MgGraph -AccessToken $(Get-AuthToken -User $User -ClientId '1b730954-1685-4b74-9bfd-dac224a7b894') # client ID of AzureAD app 
 . $ScriptDir/Import-AADObjects.ps1 -SettingsFile "$ConfigPath\JSON\AAD\Objects.json"
 
-# Reset Auth token
-$Global:PSDefaultParameterValues["*:AuthHeader"] = $(Get-AuthHeader -Token (Get-AuthToken -User $User)) # Client Id of Microsoft Intune App
+Connect-MgGraph -AccessToken $(Get-AuthToken -User $User)
+Write-Host "Adding Device Configuration Profiles"
+. $ScriptDir/Import-ConfigurationProfiles.ps1 -ImportPath "$ConfigPath\JSON\ConfigurationProfiles"
+Start-Sleep -s 5
+
+Write-Host "Adding Device Compliance Policies"
+. $ScriptDir/Import-CompliancePolicies.ps1 -ImportPath "$ConfigPath\JSON\CompliancePolicies"
+Start-Sleep -s 5
+
+# MsGraph stuff
+write-host "Adding Enrollment Status Page"
+. $ScriptDir/Import-EnrollmentStatusPage.ps1 -ImportPath "$ConfigPath\JSON\EnrollmentStatusPage"
+Start-Sleep -s 5
+
+write-host "Adding AutoPilot Profile"
+. $ScriptDir/Import-DeploymentProfiles.ps1 -ImportPath "$ConfigPath\JSON\DeploymentProfiles"
+Start-Sleep -s 5
+
+# Write-Host "Adding ADMX Device settings"
+# . $ScriptDir/Import-ConfigurationProfilesADMX.ps1 -ImportPath "$ConfigPath\JSON\DeviceConfigurationADMX" -AADGroup 'SAW-Devices-UserDriven'
+# Start-Sleep -s 5
+
+# Write-Host "Adding PS1 Config scripts"
+# . $ScriptDir/Import-DeviceConfigScript.ps1 -ImportPath "$ConfigPath\JSON\DeviceManagementScripts"
+# Start-Sleep -s 5
+
+#write-host "Adding Device Enrollment Restrictions"
+#. $ScriptDir/DER-Import_PAW.ps1
+#Start-Sleep -s 5
 
 # Write-Host "Creating Scope tag"
 # Connect-MgGraph -AccessToken $(Get-AuthToken -User $User)
@@ -49,33 +68,3 @@ $Global:PSDefaultParameterValues["*:AuthHeader"] = $(Get-AuthHeader -Token (Get-
 #         New-MgDeviceManagementRoleScopeTag -DisplayName 'Privileged-Identity' -Description 'Tag for privileged identities'
 #     }
 # }
-
-Connect-MgGraph -AccessToken $(Get-AuthToken -User $User)
-Write-Host "Adding Device Configuration Profiles"
-. $ScriptDir/Import-ConfigurationProfiles.ps1 -ImportPath "$ConfigPath\JSON\ConfigurationProfiles"
-Start-Sleep -s 5
-
-Write-Host "Adding Device Compliance Policies"
-. $ScriptDir/Import-CompliancePolicies.ps1 -ImportPath "$ConfigPath\JSON\CompliancePolicies"
-Start-Sleep -s 5
-
-# Write-Host "Adding ADMX Device settings"
-# . $ScriptDir/Import-ConfigurationProfilesADMX.ps1 -ImportPath "$ConfigPath\JSON\DeviceConfigurationADMX" -AADGroup 'SAW-Devices-UserDriven'
-# Start-Sleep -s 5
-
-# Write-Host "Adding PS1 Config scripts"
-# . $ScriptDir/Import-DeviceConfigScript.ps1 -ImportPath "$ConfigPath\JSON\DeviceManagementScripts"
-# Start-Sleep -s 5
-
-# MsGraph stuff
-write-host "Adding Enrollment Status Page"
-. $ScriptDir/Import-EnrollmentStatusPage.ps1 -ImportPath "$ConfigPath\JSON\EnrollmentStatusPage"
-Start-Sleep -s 5
-
-write-host "Adding AutoPilot Profile"
-. $ScriptDir/Import-DeploymentProfiles.ps1 -ImportPath "$ConfigPath\JSON\DeploymentProfiles"
-Start-Sleep -s 5
-
-#write-host "Adding Device Enrollment Restrictions"
-#. $ScriptDir/DER-Import_PAW.ps1
-#Start-Sleep -s 5
--- Code/decomFunctions.ps1
+++ Code/decomFunctions.ps1
@@ -255,3 +255,414 @@ Function Get-DeviceCompliancePolicy {
         Write-Error $responseBody 
     }
 }
+
+Function Add-DeviceCompliancePolicy {
+    <#
+    .SYNOPSIS
+    This function is used to add a device compliance policy using the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and adds a device compliance policy
+    .EXAMPLE
+    Add-DeviceCompliancePolicy -JSON $JSON
+    Adds an iOS device compliance policy in Intune
+    .NOTES
+    NAME: Add-DeviceCompliancePolicy
+    #>
+    [cmdletbinding()]
+    param(
+        $JSON,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [hashtable]$AuthHeader
+    )
+
+    $graphApiVersion = "Beta"
+    $Resource = "deviceManagement/deviceCompliancePolicies"
+
+    try {
+        if ($JSON -eq "" -or $JSON -eq $null) {
+            Write-Host "No JSON specified, please specify valid JSON for the iOS Policy..." -f Red
+        }
+        else {
+            Test-JSON -JSON $JSON
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+            Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Post -Body $JSON -ContentType "application/json" | Out-Null
+        }
+    }
+    catch {
+        $reader = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd()
+        Write-Error $responseBody 
+    }
+}
+
+
+Function Add-DeviceCompliancePolicyAssignment {
+    <#
+    .SYNOPSIS
+    This function is used to add a device compliance policy assignment using the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and adds a device compliance policy assignment
+    .EXAMPLE
+    Add-DeviceCompliancePolicyAssignment -ComplianceAssignments $ComplianceAssignments -CompliancePolicyId $CompliancePolicyId
+    Adds a device compliance policy assignment in Intune
+    .NOTES
+    NAME: Add-DeviceCompliancePolicyAssignment
+    #>
+
+    [cmdletbinding()]
+    param(
+        $CompliancePolicyId,
+        $Assignments,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [hashtable]$AuthHeader
+    )
+
+    $graphApiVersion = "v1.0"
+    $Resource = "deviceManagement/deviceCompliancePolicies/$CompliancePolicyId/assign"
+
+    try {
+        $JSON = @{
+            Assignments = @($ComplianceAssignments)
+        } | ConvertTo-Json -depth 5
+        Write-Output $JSON
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Post -Body $JSON -ContentType "application/json" | Out-Null
+    }
+
+    catch {
+        $reader = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd()
+        Write-Error $responseBody 
+    }
+}
+
+Function Create-GroupPolicyConfigurations {
+    <#
+    .SYNOPSIS
+    This function is used to add an device configuration policy using the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and adds a device configuration policy
+    .EXAMPLE
+    Add-DeviceConfigurationPolicy -JSON $JSON
+    Adds a device configuration policy in Intune
+    .NOTES
+    NAME: Add-DeviceConfigurationPolicy
+    #>
+    [cmdletbinding()]
+    param(
+        $DisplayName,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [hashtable]$AuthHeader
+    )
+    $jsonCode = @{
+        description = $null
+        displayName = $DisplayName
+    } | ConvertTo-Json
+
+    $graphApiVersion = "Beta"
+    $DCP_resource = "deviceManagement/groupPolicyConfigurations"
+    Write-Verbose "Resource: $DCP_resource"
+    try {
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
+        $responseBody = Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Post -Body $jsonCode -ContentType "application/json"
+    }
+    catch {
+        $reader = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd()
+        Write-Error $responseBody 
+    }
+    $responseBody.id
+}
+
+Function Create-GroupPolicyConfigurationsDefinitionValues {
+    <#
+    .SYNOPSIS
+    This function is used to get device configuration policies from the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and gets any device configuration policies
+    .EXAMPLE
+    Get-DeviceConfigurationPolicy
+    Returns any device configuration policies configured in Intune
+    .NOTES
+    NAME: Get-GroupPolicyConfigurations
+    #>
+
+    [cmdletbinding()]
+    Param (
+        [string]$GroupPolicyConfigurationID,
+        $JSON,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [hashtable]$AuthHeader
+    )
+    $graphApiVersion = "Beta"
+    $DCP_resource = "deviceManagement/groupPolicyConfigurations/$($GroupPolicyConfigurationID)/definitionValues"
+    Write-Verbose $DCP_resource
+    try {
+        if ($JSON -eq "" -or $JSON -eq $null) {
+            Write-Error "No JSON specified, please specify valid JSON for the Device Configuration Policy..."
+        }
+        else {
+            Test-JSON -JSON $JSON
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
+            Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Post -Body $JSON -ContentType "application/json" | Out-Null
+        }
+    }
+    catch {
+        $reader = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd()
+        Write-Error $responseBody 
+    }
+}
+
+Function Get-GroupPolicyConfigurations {
+    <#
+    .SYNOPSIS
+    This function is used to get device configuration policies from the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and gets any device configuration policies
+    .EXAMPLE
+    Get-DeviceConfigurationPolicy
+    Returns any device configuration policies configured in Intune
+    .NOTES
+    NAME: Get-GroupPolicyConfigurations
+    #>
+    [cmdletbinding()]
+    param(
+        $name,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [hashtable]$AuthHeader
+    )
+    $graphApiVersion = "Beta"
+    $DCP_resource = "deviceManagement/groupPolicyConfigurations"
+
+    try {
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
+		(Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Get).Value | Where-Object { $_.displayName -eq $Name }
+    }
+    catch {
+        $reader = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd()
+        Write-Error $responseBody 
+    }
+}
+
+Function Add-GroupPolicyConfigurationPolicyAssignment {
+    <#
+    .SYNOPSIS
+    This function is used to add a device configuration policy assignment using the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and adds a device configuration policy assignment
+    .EXAMPLE
+    Add-DeviceConfigurationPolicyAssignment -ConfigurationPolicyId $ConfigurationPolicyId -TargetGroupId $TargetGroupId
+    Adds a device configuration policy assignment in Intune
+    .NOTES
+    NAME: Add-DeviceConfigurationPolicyAssignment
+    #>
+    [cmdletbinding()]
+    param(
+        $ConfigurationPolicyId,
+        $TargetGroupId,
+        $Assignment,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [hashtable]$AuthHeader
+    )
+
+    $graphApiVersion = "Beta"
+    $Resource = "deviceManagement/groupPolicyConfigurations/$ConfigurationPolicyId/assignments"
+
+    try {
+        if (!$ConfigurationPolicyId) {
+            Write-Error "No Configuration Policy Id specified, specify a valid Configuration Policy Id"
+            break
+        }
+        if (!$TargetGroupId) {
+            Write-Error "No Target Group Id specified, specify a valid Target Group Id"
+            break
+        }
+        if (!$Assignment) {
+            Write-Error "No Assignment Type specified, specify a valid Assignment Type"
+            break
+        }
+        $JSON = @{
+            target = @{
+                "@odata.type" = "#microsoft.graph.$Assignment"
+                groupId       = $TargetGroupId
+            }
+        } | ConvertTo-Json
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Post -Body $JSON -ContentType "application/json" | Out-Null
+    }
+    catch {
+        $reader = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd()
+        Write-Error $responseBody 
+    }
+}
+
+Function Get-DeviceManagementScript {
+    <#
+    .SYNOPSIS
+    This function is used to get device management scripts from the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and gets any device management scripts
+    .EXAMPLE
+    Get-DeviceManagementScript
+    Returns any device management scripts configured in Intune
+    .NOTES
+    NAME: Get-DeviceManagementScript
+    #>
+    [cmdletbinding()]
+    param(
+        $name,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [hashtable]$AuthHeader
+    )
+    $graphApiVersion = "Beta"
+    $DCP_resource = "deviceManagement/deviceManagementScripts"
+    try {
+        if ($Name) {
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
+            $DevMgmtScript = (Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Get).Value | Where-Object { ($_.'displayName').contains($Name) }
+            if ($DevMgmtScript) {
+                $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)/$($DevMgmtScript.id)"
+                Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Get
+            }
+        }
+        else {
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
+            (Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Get).Value
+        }
+    }
+    catch {
+        $reader = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd()
+        Write-Error $responseBody 
+    }
+}
+
+Function Add-DeviceManagementScript {
+    <#
+    .SYNOPSIS
+    This function is used to add a device management script using the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and adds a device management script
+    .EXAMPLE
+    Add-DeviceManagementScript -File "path to powershell-script file"
+    Adds a device management script from a File in Intune
+    Add-DeviceManagementScript -File "URL to powershell-script file" -URL
+    Adds a device management script from a URL in Intune
+    .NOTES
+    NAME: Add-DeviceManagementScript
+    #>
+    [cmdletbinding()]
+    param(
+        $JSON,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [hashtable]$AuthHeader
+    )
+
+    # $B64File = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($File))
+
+    $graphApiVersion = "Beta"
+    $DMS_resource = "deviceManagement/deviceManagementScripts"
+    Write-Verbose "Resource: $DMS_resource"
+
+    try {
+        if ($JSON -eq "" -or $JSON -eq $null) {
+            Write-Host "No JSON specified, please specify valid JSON for the Android Policy..." -f Red
+        }
+        else {
+            Test-JSON -JSON $JSON
+            $uri = "https://graph.microsoft.com/$graphApiVersion/$($DMS_resource)"
+            Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Post -Body $JSON -ContentType "application/json"
+        }
+    }
+    catch {
+        $reader = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd()
+        Write-Error $responseBody 
+    }
+}
+
+Function Add-DeviceManagementScriptAssignment {
+    <#
+    .SYNOPSIS
+    This function is used to add a device configuration policy assignment using the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and adds a device configuration policy assignment
+    .EXAMPLE
+    Add-DeviceConfigurationPolicyAssignment -ConfigurationPolicyId $ConfigurationPolicyId -TargetGroupId $TargetGroupId
+    Adds a device configuration policy assignment in Intune
+    .NOTES
+    NAME: Add-DeviceConfigurationPolicyAssignment
+    #>
+
+    [cmdletbinding()]
+    param(
+        $ScriptId,
+        $TargetGroupId,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [hashtable]$AuthHeader
+    )
+
+    $graphApiVersion = "Beta"
+    $Resource = "deviceManagement/deviceManagementScripts/$ScriptId/assign"
+
+    try {
+        if (!$ScriptId) {
+            Write-Error "No Script Policy Id specified, specify a valid Script Policy Id"
+            break
+        }
+
+        if (!$TargetGroupId) {
+            Write-Error "No Target Group Id specified, specify a valid Target Group Id"
+            break
+        }
+
+        $JSON = [ordered]@{
+            deviceManagementScriptGroupAssignments = @(
+                @{
+                    "@odata.type" = "#microsoft.graph.deviceManagementScriptGroupAssignment"
+                    targetGroupId = $TargetGroupId
+                    id            = $ScriptId
+                }
+            )
+        } | ConvertTo-Json
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$Resource"
+        Invoke-RestMethod -Uri $uri -Headers $AuthHeader -Method Post -Body $JSON -ContentType "application/json"
+    }
+    catch {
+        $reader = New-Object System.IO.StreamReader($_.Exception.Response.GetResponseStream())
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd()
+        Write-Error $responseBody 
+    }
+}
